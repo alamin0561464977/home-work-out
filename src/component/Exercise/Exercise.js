f3699dc3b7384e6c9810a9298f030e409d924be3
alamin0561464977 <amin23435678901315@gmail.com>
2022-09-29 10:28:06 +0300

    added a blog how does react js works?

--- src/component/Exercise/Exercise.js
+++ src/component/Exercise/Exercise.js
@@ -26,6 +26,18 @@ const Exercise = ({ addToCard }) => {
                     ></ExerciseCard>)
                 }
             </div>
+            <div className="row mt-5 row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 g-4">
+                <div>
+                    <div className="card text-bg-secondary mb-3" style={{ maxWidth: '18rem' }}>
+                        <div className="card-body">
+                            <h4 className="card-title">how does react js works?</h4>
+                            <hr />
+                            <p className="card-text">React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
